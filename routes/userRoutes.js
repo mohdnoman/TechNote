f0d5099471ac88006d2aab256bc6199b82e6915e
mohd noman <mohdnoman2751@gmail.com>
2024-05-03 23:44:03 +0530

    implimented user authentication and authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersControllers")
+const verifyJWT = require("../middleware/verifyJWT")
+
+router.use(verifyJWT)
+
 router.route("/")
     .get(usersController.getAllUsers)
     .post(usersController.createNewUser)
