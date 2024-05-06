f30bfc33ff732ead347b227245993762ac5ba3ff
mohd noman <mohdnoman2751@gmail.com>
2024-05-06 23:54:31 +0530

    implimented role based access.

--- frontend/src/features/auth/PersistLogin.js
+++ frontend/src/features/auth/PersistLogin.js
@@ -49,7 +49,7 @@ const PersistLogin = () => {
     } else if (isError) {
         console.log("error")
         content = (
-            <p className="errmsg">{error.data?.message}
+            <p className="errmsg">{error?.data?.message}
                 <Link to="/login">Please login again</Link>
             </p>
         )
