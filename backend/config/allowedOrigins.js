9dcb7b58b4d8936a0783ef9a6c0b879809f51e4f
mohd noman <mohdnoman2751@gmail.com>
2024-05-16 14:40:44 +0530

    cros origion update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://www.dandrepairshop.com',
     'https://dandrepairshop.com',
-    "https://technotes27.netlify.app/"
+    "https://technotes27.netlify.app"
 ]
 
 module.exports = allowedOrigins
